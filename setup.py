1b216e8f3543ebdf09bf41dde87b5b6eb23a6335
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-01-27 01:39:59 +0100

    added requirements to the version 1.0.4

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 import setuptools
+import pkg_resources
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = list(map(str, pkg_resources.parse_requirements(fh.read())))
+
 setuptools.setup(
     name="models-gamare",
-    version="1.0.3",
+    version="1.0.4",
     author="Gabriel Amare",
     author_email="gabriel.amare.31@gmail.com",
     description="Package to help implement complex object structures within your project",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    install_requires=requirements,
     url="https://github.com/GabrielAmare/Models",
     packages=['models', 'models.utils'],
     classifiers=[
