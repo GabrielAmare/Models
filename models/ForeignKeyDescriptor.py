64a169465a9fb11f055f43c0120baaa86f617007
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-01-29 15:07:07 +0100

    patch ForeignKey type

--- models/ForeignKeyDescriptor.py
+++ models/ForeignKeyDescriptor.py
@@ -8,7 +8,7 @@ class ForeignKeyDescriptor(AttributeDescriptor):
         :param get_by: if specified (tuple of 2 elements, start & step) auto-increment the field value when unspecified
         :param on_lazy: [optional] set a range of authorized values (ex: (0, 1000) )
         """
-        super().__init__(name, f"{get_from}.{get_by}", unique=not config.get('multiple', False), static=False, **config)
+        super().__init__(name, get_from, unique=not config.get('multiple', False), static=False, **config)
         self.get_from = get_from
         self.get_by = get_by
         self.on_lazy = on_lazy
